ffff3aec08757ba03e7eeefc3d3743c2ceacfafa
Aabishkar Aryal <aabiskar1@live.com>
2020-05-19 18:26:35 +0545

    up to number 7 finshed

---
+++ DVDRentalSystem/Controllers/ReturnDVDController.cs
@@ -0,0 +1,28 @@
+﻿using DataContext.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DVDRentalSystem.Controllers
+{
+    public class ReturnDVDController : Controller
+    {
+        private DVDRentalSystemContext dbCon = new DVDRentalSystemContext();
+        // GET: ReturnDVD
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ReturnDVD() {
+            var movieList = dbCon.DVDDetails.Select(x => x.Name);
+            ViewBag.MovieLists = movieList.ToList();
+
+
+
+            return View();
+        }
+
+    }
+}
---
+++ DVDRentalSystem/Controllers/ReturnDVDPostController.cs
@@ -0,0 +1,74 @@
+﻿using DataContext.Data;
+using DVDRentalSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DVDRentalSystem.Controllers
+{
+    public class ReturnDVDPostController : Controller
+    {
+        private DVDRentalSystemContext dbCon = new DVDRentalSystemContext();
+        // GET: ReturnDVDPost
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult CopyIdList(string dvdName)
+        {
+            dvdName = HttpContext.Request["DVDDetails"];
+            var copyNumber = dbCon.Loans.Include("DVDDetails").Where(x => x.DVDDetails.Name == dvdName).Where(x => x.ActualReturnedDate == null).Select(x => x.CopyId);
+            ViewBag.CopyId = copyNumber.ToList();
+
+
+            TempData["ReturnDVDName"] = dvdName;
+
+            return View();
+
+        }
+
+        public ActionResult ShowReturnDetails(string copyid)
+        {
+
+
+            string dvdName = TempData["ReturnDVDName"] as string;
+
+            TempData["ReturnDVDName"] = dvdName;
+            TempData["ReturnCopyID"] = copyid;
+
+            var loanReport = dbCon.Loans.Include("DVDDetails").Where(x => x.DVDDetails.Name == dvdName).Where(x=> x.ActualReturnedDate== null);
+
+
+
+           
+            return View(loanReport);
+        }
+
+
+
+        public ActionResult ReturnDVDFinal()
+        {
+            string dvdName = TempData["ReturnDVDName"] as string;
+            string copyId = TempData["ReturnCopyID"] as string;
+            int parsedCopyId = int.Parse(copyId);
+
+            var loanedRow = dbCon.Loans.Where(x => x.DVDDetails.Name == dvdName)
+                .Where(x => x.ActualReturnedDate == null)
+                .Where(x => x.CopyId == parsedCopyId).SingleOrDefault();
+
+            if (loanedRow != null)
+            {
+                loanedRow.ActualReturnedDate = DateTime.Now;
+
+                dbCon.SaveChanges();
+            }
+
+            return RedirectToAction("index", "Loans");
+        }
+    }
+}
