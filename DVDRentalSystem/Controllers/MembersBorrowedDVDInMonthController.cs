98de435e100401bbe3fa5be1cb8c90cab547d235
Aabishkar Aryal <aabiskar1@live.com>
2020-05-20 20:11:52 +0545

    solved question 12 (remaining: 9,13,14)

---
+++ DVDRentalSystem/Controllers/MembersBorrowedDVDInMonthController.cs
@@ -0,0 +1,44 @@
+﻿using DataContext.Data;
+using DVDRentalSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DVDRentalSystem.Controllers
+{
+    public class MembersBorrowedDVDInMonthController : Controller
+    {
+        private DVDRentalSystemContext db = new DVDRentalSystemContext();
+        // GET: MembersBorrowedDVDInMonth
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult BorrowedDVDInLastMonth()
+        {
+            var loanedMemberList = db.Loans.Include("Members");
+
+
+            List<int> allMemberInLoanList = new List<int>();
+            List<int> distinctMemberId = new List<int>();
+            List<Loan> loanUserListLatestIssue = new List<Loan>();
+       
+            foreach (var item in loanedMemberList)
+            { allMemberInLoanList.Add(item.MemberId); }
+
+            distinctMemberId = allMemberInLoanList.Distinct().ToList();
+
+
+            foreach (var uniqueId in distinctMemberId)
+            {
+                loanUserListLatestIssue = db.Loans.Include("Members").Where(x => x.MemberId == uniqueId).OrderByDescending(x=>x.IssueDate).Take(1).ToList();
+                               
+            }
+
+            return View(loanUserListLatestIssue);
+        }
+    }
+}
